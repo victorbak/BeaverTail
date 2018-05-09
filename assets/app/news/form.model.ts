87f70075efa235c185fcbd07b357b1333a9e9b67
kunlayako <kunlayajwry@hotmail.com>
2018-05-08 17:14:06 -0700

    form submission

---
+++ assets/app/news/form.model.ts
@@ -0,0 +1,12 @@
+export class Form {
+
+    constructor(
+        public urlSubmission: string,
+        public synopsis: string,
+        public contryTag: string,
+        public governmentTag: string,
+        public peopleTag: string,
+        public generalTag: string
+        
+    ){}
+}

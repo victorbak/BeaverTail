95c9298fbcf6a520d70b928a8a8df2797a398085
victorbak <victor.bak2@gmail.com>
2018-05-10 05:54:44 -0700

    add and get service added- bugged

--- assets/app/news/news.model.ts
+++ assets/app/news/news.model.ts
@@ -1,4 +1,4 @@
-export class Form {
+export class News {
 
     constructor(
         public urlSubmission: string,
--- assets/app/news/news.service.ts
+++ assets/app/news/news.service.ts
@@ -1,7 +1,45 @@
 import { Injectable } from "@angular/core";
-
+import { Http, Response, Headers } from "@angular/http";
+import { News } from "./news.model"
+import "rxjs/Rx";
+import { Observable } from "rxjs"
 
 @Injectable()
 export class NewsService {
+    private stories: News[] = [];
 
+    constructor(private http: Http) {}
+
+    addNews(news: News) {
+        const body = JSON.stringify(news);
+        const headers = new Headers({'Content-Type': 'application/json'})
+        return this.http.post('http://localhost:3000/news', body, {headers: headers})
+            .map((response: Response) => {
+                const result = response.json();
+                const news = new News(result.obj.title,
+                                      result.obj.synopsis,
+                                      'Canada',
+                                      'Govmnt',
+                                      'ppl',
+                                      result.obj.tags
+                                      );
+                this.stories.push(news);
+                return news;
+            })
+            .catch((error: Response) => Observable.throw(error.json()));
+    }
+
+    getNews() {
+        return this.http.get('http://localhost:3000/news')
+            .map((response: Response) => {
+                const stories = response.json().obj;
+                let transformedNews: News[] = [];
+                for (let news of stories) {
+                    transformedNews.push(new News(news.title, news.synopsis, 'Canada', 'Govmnt', 'ppl', 'tag'));
+                }
+                this.stories = transformedNews;
+                return transformedNews;
+            })
+            .catch((error: Response) => Observable.throw(error.json()));            
+    }
 }

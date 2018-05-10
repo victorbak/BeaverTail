9583fa46216f37af8e62bceeddf7284ac872d22e
kunlayako <kunlayajwry@hotmail.com>
2018-05-10 13:53:16 -0700

    moved form to just one page

--- assets/app/shared/header.component.ts
+++ assets/app/shared/header.component.ts
@@ -4,7 +4,10 @@ import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-header',
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
+    styleUrls:[
+        './header.component.css'
+    ]
 })
 export class HeaderComponent implements OnInit {
 

9dc88977048fac2ec319bab3079f982c5b8ed127
Robbie Perry <robbieperry22@gmail.com>
2018-05-15 16:28:01 -0700

    Points populating from DB now works, as well as clustering

--- assets/app/maps/main-map.component.ts
+++ assets/app/maps/main-map.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { NewsService } from "../news/news.service";
+import { News } from "../news/news.model";
+import { MarkerManager } from "@agm/core";
 
 @Component({
     selector: 'app-main-map',
@@ -7,6 +10,34 @@ import { Component } from "@angular/core";
         './main-map.component.css'
     ]
 })
-export class MainMapComponent {
+export class MainMapComponent implements OnInit {
+    
+    news: News[]
+    markers: Markers[] = []
 
+    constructor(private newsService: NewsService) {}
+
+    ngOnInit() {
+        this.newsService.getNews().subscribe(
+            (news: News[]) => {
+                this.news = news
+                this.news.forEach(n => {
+                    this.markers.push({
+                        lat: n.latitude,
+                        lng: n.longitude,
+                        title: n.title,
+                        id: n.newsId
+                    })
+                })
+                console.log(this.news)
+            }
+        )
+    }
+}
+
+interface Markers {
+    lat: number
+    lng: number
+    title: string
+    id: string
 }

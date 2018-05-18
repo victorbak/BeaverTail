6dd5f4da4d781a49dd1fcd28715c342d263fe8ad
LuLu <shelu428@gmail.com>
2018-05-17 21:25:31 -0700

    deleted useless css

--- assets/app/shared/header.component.ts
+++ assets/app/shared/header.component.ts
@@ -7,16 +7,7 @@ import { FlexLayoutModule } from "@angular/flex-layout";
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls:[
-        './header.component.css',
-    './animate.component.css',
-    '/bootstrap.component.css',
-    '/flexslider.component.css',
-    '/icomoon.component.css',
-    '/magnific-popup.component.css',
-    '/owl.carousel.min.component.css',
-    '/owl.theme.default.min.component.css',
-    '/style.component.css',
-    '/themify-icons.component.css',
+        './header.component.css'
     ]
 })
 export class HeaderComponent implements OnInit{

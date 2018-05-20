22d6b7c4829f784d7111ab310633e385292fdecc
kunlayako <kunlayajwry@hotmail.com>
2018-05-20 05:02:18 -0700

    edited about us page

--- assets/app/aboutus/about-us.component.ts
+++ assets/app/aboutus/about-us.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'about-us',
     templateUrl: './about-us.component.html',
-    styles: ['./about-us.component.css'],
+    styleUrls: ['./about-us.component.css'],
 
 })
 export class aboutUs{

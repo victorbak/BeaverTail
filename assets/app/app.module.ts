d80b7652accbf6646ed9139eca08aff48d433354
LuLu <shelu428@gmail.com>
2018-05-23 00:41:20 -0700

    routing after each action

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from './shared/header.component';
 import { Ng4GeoautocompleteModule } from 'ng4-geoautocomplete'
 import { SignupComponent } from '../app/auth/signup.component';
-import { routing } from './app.routing';
 import { SigninComponent } from '../app/auth/signin.component';
 import { HttpModule } from '@angular/http';
 import { AuthService } from './auth/auth.service';
+import { routing } from './app.routing';
 import { AgmCoreModule } from '@agm/core'
 import { MainMapComponent } from './maps/main-map.component';
 import { ProfileComponent } from './user/profile.component';
